c97ced1ac1e101553758995a5bf3a55df7e997ee
xuyd <sssxyd@gmail.com>
2025-03-20 14:34:56 +0800

    1

--- build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let dll_dir = "win_x64";
-    println!("cargo:rustc-link-search=native={}", dll_dir);
-}
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ use std::path::PathBuf;
 use rofd::{export_ofd_to_png, read_ofd};
 
 fn main() {
-    let dll_path = PathBuf::from(env::current_dir().unwrap()).join("win_x64");
-    env::set_var("PATH", format!("{};{}", dll_path.display(), env::var("PATH").unwrap()));
-    // 后续代码...
 
     let ofd_node = read_ofd("./learning/test.ofd").unwrap();
     println!("ofd: {:#?}", ofd_node);
